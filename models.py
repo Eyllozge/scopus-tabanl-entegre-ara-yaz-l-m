915e9eee0bc328a7c276dbc6373c7ab73db59267
ozge <eyllozge.1210@gmail.com>
2026-07-24 14:49:36 +0300

    models dosyası ve ona bağlı veritabanı şemaları oluşturuldu. test scopustan verinin nasıl geldiğini öğrenmek için dahil edildi sonradan çıkarılacak.

---
+++ models.py
@@ -0,0 +1,46 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, Table
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+article_author_association = Table(
+    'article_author', Base.metadata,
+    Column('article_id', Integer, ForeignKey('articles.id')),
+    Column('author_id', Integer, ForeignKey('authors.id'))
+)
+
+article_institution_association = Table(
+    'article_institution', Base.metadata,
+    Column('article_id', Integer, ForeignKey('articles.id')),
+    Column('institution_id', Integer, ForeignKey('institutions.id'))
+)
+
+class Article(Base):
+    __tablename__ = 'articles'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    scopus_id = Column(String, unique=True, index=True, nullable=False) 
+    art_name = Column(String, nullable=False) 
+    publication_name = Column(String) 
+    cover_date = Column(Date)
+    doi = Column(String, unique=True, nullable=True) 
+    citedby_count = Column(Integer, default=0) 
+    
+    authors = relationship("Author", secondary=article_author_association, back_populates="articles")
+    institutions = relationship("Institution", secondary=article_institution_association, back_populates="articles")
+
+class Author(Base):
+    __tablename__ = 'authors'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    auth_fullname = Column(String, unique=True, index=True, nullable=False) 
+    
+    articles = relationship("Article", secondary=article_author_association, back_populates="authors")
+
+class Institution(Base):
+    __tablename__ = 'institutions'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    institution_name = Column(String, unique=True, index=True, nullable=False) 
+    
+    articles = relationship("Article", secondary=article_institution_association, back_populates="institutions")
---
+++ test.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+API_KEY = "a3836c4d84b930e321232186e37392dd"
+
+URL = "https://api.elsevier.com/content/search/scopus"
+
+
+params = {
+    "query": "AFFIL(Firat University) AND PUBYEAR IS 2024",
+    "count": 3,  
+    "view": "STANDARD"  
+}
+
+
+headers = {
+    "X-ELS-APIKey": API_KEY,
+    "Accept": "application/json"  
+}
+
+print("Scopus API'sine istek atılıyor...")
+
+try:
+    response = requests.get(URL, headers=headers, params=params)
+    
+
+    if response.status_code == 200:
+        data = response.json()
+        
+    
+        print("\n--- BAŞARILI! GELEN JSON VERİSİ ---\n")
+        print(json.dumps(data, indent=4, ensure_ascii=False))
+        
+    else:
+        print(f"\nHata! Durum Kodu: {response.status_code}")
+        print("Hata Detayı:", response.text)
+
+except Exception as e:
+    print(f"\nİstek sırasında bir hata oluştu: {e}")
