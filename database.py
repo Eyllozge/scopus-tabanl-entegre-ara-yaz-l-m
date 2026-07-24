1183b25e0797b7d78e7532f62c3c55898e918f65
ozge <eyllozge.1210@gmail.com>
2026-07-24 15:35:45 +0300

    Database dosyası oluşturuldu.

---
+++ database.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+SQLALCHEMY_DB_URL = os.getenv("DB_URL")
+
+engine = create_engine(
+    SQLALCHEMY_DB_URL, 
+    pool_pre_ping=True 
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
