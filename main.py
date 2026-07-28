6858debb0ffb1240554d832f9f4b54f6dd748253
ozge <eyllozge.1210@gmail.com>
2026-07-28 11:14:42 +0300

    düzenleme

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ app = FastAPI(
 # CORS yapısı
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://scpyazilim.vercel.app/"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],
