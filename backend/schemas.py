f722d66f8445d2125ff320b8f16f7755eb5a6753
ozge <eyllozge.1210@gmail.com>
2026-08-04 15:09:43 +0300

    tasarım güncellemeleri

--- backend/schemas.py
+++ backend/schemas.py
@@ -5,28 +5,30 @@ from typing import Optional, List
 # 1. Yazar için alt şema
 class AuthorResponse(BaseModel):
     auth_fullname: str
-    
+
     class Config:
         from_attributes = True
 
 # 2. Kurum için alt şema
 class InstitutionResponse(BaseModel):
     institution_name: str
-    
+
     class Config:
         from_attributes = True
 
-# 3. Ana Makale Şeması 
+# 3. Ana Makale Şeması
 class ArticleResponse(BaseModel):
-    id : int
+    id: int
     scopus_id: str
     art_name: str
     publication_name: Optional[str]
     cover_date: Optional[date]
+    doi: Optional[str] = None          
     citedby_count: int
     abstract: Optional[str] = None
     keywords: Optional[str] = None
     authors: List[AuthorResponse] = []
     institutions: List[InstitutionResponse] = []
+
     class Config:
         from_attributes = True
